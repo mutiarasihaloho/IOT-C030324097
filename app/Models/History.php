ab365ef3086d84df7ef2983e296b30bb41fd2c91
mutiara <mutiarasihalohoxx8@gmail.com>
2025-10-21 23:19:44 +0800

    Model History

---
+++ app/Models/History.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\Hasfactory;
+use Illuminate\Database\Eloquent\Model;
+
+class History extends Model
+{
+    use Hasfactory;
+    protected $fillable = ['lamp_id', 'status'];
+
+    public function lamp()
+    {
+        return $this->belongsTo(lamp::class);
+    }
+}
---
+++ app/Models/Lamp.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lamp extends Model
+{
+    use HasFactory;
+
+    /**
+     * fillable
+    *
+    * @var array
+    */
+    protected $fillable = ['name', 'status'];
+
+    public function histories();
+    {
+        return $this->hasMany(History::class);
+    }
+}
